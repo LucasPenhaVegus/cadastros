df7125fc3b5ebf0e64094e877704fd7ded5895da
HidekiLucas0701 <Hideki.lucas0701@gmail.com>
2024-11-22 15:32:09 -0300

    WIP: Atribuir funcionalidade ao botão Ver Senha

--- cadastroUsuario.js
+++ cadastroUsuario.js
@@ -1,35 +1,40 @@
 
 
-document.getElementById("formCadastro").addEventListener("submit", function(e) {
-    e.preventDefault();
+const formCadastro = document.getElementById("formCadastro");
+if (formCadastro) {
 
-    const nome = document.getElementById("nome").value.trim();
-    const sobrenome = document.getElementById("sobrenome").value.trim();
-    const cpf = document.getElementById("cpf").value.trim();
-    const nascimento = document.getElementById("nascimento").value.trim();
-    const usuario = document.getElementById("usuario").value.trim();
-    const senha = document.getElementById("senha").value.trim();
-    const confirmaSenha = document.getElementById("confirmaSenha").value.trim();
+    formCadastro.addEventListener("submit", function(e) {
+        e.preventDefault();
 
-    if (senha !== confirmaSenha) {
-        document.getElementById("errorMessage").textContent = "As senhas devem ser iguais!";
-    } else {
-        document.getElementById("errorMessage").textContent = "";
-        alert("Usuário cadastrado com sucesso!");
-    }
+        const nome = document.getElementById("nome").value.trim();
+        const sobrenome = document.getElementById("sobrenome").value.trim();
+        const cpf = document.getElementById("cpf").value.trim();
+        const nascimento = document.getElementById("nascimento").value.trim();
+        const usuario = document.getElementById("usuario").value.trim();
+        const senha = document.getElementById("senha").value.trim();
+        const confirmaSenha = document.getElementById("confirmaSenha").value.trim();
 
-});
+        if (senha !== confirmaSenha) {
+            document.getElementById("errorMessage").textContent = "As senhas devem ser iguais!";
+        } else {
+            document.getElementById("errorMessage").textContent = "";
+            alert("Usuário cadastrado com sucesso!");
+        }
 
-document.getElementById("verSenha").addEventListener("click", function () {
-    const senhaUsuario = document.getElementById("senha")
-    if (senhaUsuario.type === "password") {
-        senhaUsuario.type = "text";
-        this.textContent = "X"
-    } else {
-        senhaUsuario.type = "password";
-        this.textContent = "O"
-    }
-});
+    });
+
+    document.getElementById("verSenha").addEventListener("click", function () {
+        const senhaUsuario = document.getElementById("senha")
+        if (senhaUsuario.type === "password") {
+            senhaUsuario.type = "text";
+            this.textContent = "\u23DC"
+        } else {
+            senhaUsuario.type = "password";
+            this.textContent = "\uD83D\uDC41"
+        }
+    });
+
+}
 
 const verSenhaLogin = document.getElementById("verSenhaLogin");
 if (verSenhaLogin) {
@@ -37,10 +42,10 @@ if (verSenhaLogin) {
         const senhaUsuarioLogin = document.getElementById("senhaLogin")
         if (senhaUsuarioLogin.type === "password") {
             senhaUsuarioLogin.type = "text";
-            this.textContent = "X"
+            this.textContent = "\u23DC"
         } else {
             senhaUsuarioLogin.type = "password";
-            this.textContent = "O"
+            this.textContent = "\uD83D\uDC41"
         }
     });
 }
